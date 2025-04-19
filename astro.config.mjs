5cf1da6ad41b32d54f9168e3da45d8dfed6a9e61
JB55Matthews <blackhedley25@gmail.com>
2025-04-19 20:47:15 -0230

    site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://jbmattews.github.io",
+    site: "https://jb55mattews.github.io",
     base: "/personal-website",
 });
