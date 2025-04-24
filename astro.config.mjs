3ef34020ec89235206dd69482030fd793fec93d7
Jason Matthews <168492258+JB55Matthews@users.noreply.github.com>
2025-04-24 09:14:15 -0230

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://jb55mattews.github.io",
-    base: "/personal-website",
 });
